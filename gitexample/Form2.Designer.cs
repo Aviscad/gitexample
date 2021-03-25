d2e21b408570019a73b6feceb1ed760f9d16135c
Manuel Viscarra <36599299+Aviscad@users.noreply.github.com>
2021-03-25 10:01:44 -0600

    nuevo form

--- gitexample/Form2.Designer.cs
+++ gitexample/Form2.Designer.cs
@@ -36,7 +36,7 @@ namespace gitexample
             // 
             this.button1.Location = new System.Drawing.Point(239, 114);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(257, 170);
+            this.button1.Size = new System.Drawing.Size(396, 170);
             this.button1.TabIndex = 0;
             this.button1.Text = "Esto es otro formulario";
             this.button1.UseVisualStyleBackColor = true;
