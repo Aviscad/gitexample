5ca2999075fbf21c620651cf1d367550e01ae946
Manuel Viscarra <36599299+Aviscad@users.noreply.github.com>
2021-03-25 09:48:31 -0600

    hello world?

--- gitexample/Form1.cs
+++ gitexample/Form1.cs
@@ -16,5 +16,10 @@ namespace gitexample
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(textBox1.Text);
+        }
     }
 }
